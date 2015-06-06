227c497eecc16d5549a9fc5150f9862d8fe3314e
hypexr <scott@hypexr.org>
2015-06-06 13:44:47 -0700

    Small selection header once an initial choice is made

--- ui/app/js/app.js
+++ ui/app/js/app.js
@@ -13,12 +13,21 @@ App.Router.map(function() {
 /*
  * Routes
  */
+App.ApplicationRoute = Em.Route.extend({
+    setupController: function(controller, model) {
+        controller.set('fullHeader', true);
+    }
+});
+
 App.HospitalsRoute = Em.Route.extend({
     model: function(params) {
-        var route = this;
-
-        Em.Logger.info("paramas: ", params);
         return App.Hospital.fetch({'rating_criteria': params.criteria_id, 'county': 'Fresno'});
+    },
+
+    setupController: function(controller, model) {
+        var route = this;
+        route.controllerFor('application').set('fullHeader', false);
+        controller.set('model', model);
     }
 
     //afterModel: function() {
@@ -31,6 +40,20 @@ App.HospitalsRoute = Em.Route.extend({
 /*
  * Controllers
  */
+App.ApplicationController = Em.ArrayController.extend({
+    fullHeader: true
+});
+
+App.HospitalsController = Em.ArrayController.extend({
+    needs: ['application'],
+
+    //init: function() {
+    //    var controller = this;
+    //    Em.Logger.info("in hosp controller");
+    //    var appController = controller.get('controllers.application');
+    //    appController.set('fullHeader', false);
+    //}
+});
 
 
 /*
